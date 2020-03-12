19f0ce597d7d2c538a433f1236344912988f0216
rania235 <rania.obeid1996@gmail.com>
2020-03-12 12:25:30 +0200

    Groceries

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,19 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './src/pages/LoginPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Empty from './src/pages/emptyImage';
-import SignupPage from './src/pages/SignupPage';
+//import Empty from './src/pages/emptyImage';
+//import SignupPage from './src/pages/SignupPage';
+import Groceries from './src/pages/Groceries';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-      <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Empty" component={Empty} />
-        <Stack.Screen name="SignupPage" component={SignupPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Groceries/>
+      // <NavigationContainer>
+      //<Stack.Navigator>
+       // <Stack.Screen name="Login" component={LoginPage} />
+        //<Stack.Screen name="Empty" component={Empty} />
+        //<Stack.Screen name="SignupPage" component={SignupPage} />
+      //</Stack.Navigator>
+  //  </NavigationContainer> 
   );
 }
