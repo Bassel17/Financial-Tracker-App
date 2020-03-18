b812d0859d61d9be80c73c6d67603db7b3ea98c2
Bassel <basselkanso82@gmail.com>
2020-03-18 20:17:45 +0200

    changes

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,32 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './src/pages/LoginPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-<<<<<<< HEAD
-//import Empty from './src/pages/emptyImage';
-//import SignupPage from './src/pages/SignupPage';
-import BurgerPage from './src/pages/BurgerPage';
-=======
 import Empty from './src/pages/emptyImage';
 import SignupPage from './src/pages/SignupPage';
 import Groceries from './src/pages/Groceries';
 import OverviewPage from './src/pages/OverviewPage';
->>>>>>> overviewPage
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <BurgerPage/>
-    //<Groceries/>
-      // <NavigationContainer>
-      //<Stack.Navigator>
-       // <Stack.Screen name="Login" component={LoginPage} />
-        //<Stack.Screen name="Empty" component={Empty} />
-        //<Stack.Screen name="SignupPage" component={SignupPage} />
-      //</Stack.Navigator>
-  //  </NavigationContainer> 
-=======
       <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen name="OverviewPage" component={OverviewPage} />
@@ -37,6 +20,5 @@ export default function App() {
         <Stack.Screen name="SignupPage" component={SignupPage} />
       </Stack.Navigator>
    </NavigationContainer> 
->>>>>>> overviewPage
   );
 }
