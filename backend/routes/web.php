f879b214c2a14d2354909063ef928c46330c609d
Bassel <basselkanso82@gmail.com>
2020-03-27 16:46:10 +0200

    fixed some stuff

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,20 +17,6 @@ header('Content-type: application/json');
 Route::get('/samar', 'SendDataController@messageTosamar');
 Route::get('/bassel', 'SendDataController@messageTobassel');
 Route::get('/rania', 'SendDataController@messageToRania');
- Route::get('/samar',function(){
-    return response()->json([
-              'name' => 'samar',
-              'message' => 'you can do this, I trust you and I know you can'
-          ]);
-  });
-  Route::get('/rania',function(){
-    return response()->json([
-              'name' => 'rania',
-              'message' => 'you can do this, I trust you and I know you can'
-          ]);
-  });
-
-
 
 
 Route::get('/', function(){
