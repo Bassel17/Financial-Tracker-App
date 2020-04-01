c0de0d32fc27d7692cb0cbd5d094f27844aab6af
Bassel <basselkanso82@gmail.com>
2020-04-01 17:12:12 +0300

    problem solved

--- Frontend/src/components/Bar.js
+++ Frontend/src/components/Bar.js
@@ -1,41 +1,46 @@
-import {
-  BarChart
-} from "react-native-chart-kit";
-import React,{ Component } from "react";
-import { Dimensions,View,Text } from "react-native";
+import React from "react";
+import { ScrollView, StatusBar, Dimensions, Text, View } from "react-native";
+import { PieChart, BarChart } from "react-native-chart-kit";
 
-class Bar extends Component{
-render(){
-  const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43]
-      }
-    ]
-  };
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43]
+    }
+  ]
+};
 
-  const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5
-  };
+const chartConfig = {
+  backgroundColor: "#e26a00",
+  backgroundGradientFrom: "#fb8c00",
+  backgroundGradientTo: "#ffa726",
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 16
+  },
+  propsForDots: {
+    r: "6",
+    strokeWidth: "2",
+    stroke: "#ffa726"
+  }
+};
+export default class Bar extends React.Component {
+  render() {
+    const screenWidth = Dimensions.get("window").width;
+    const height = 220;
 
-  const screenWidth = Dimensions.get("window").width;
-  return(
-    <BarChart
-    data={data}
-    width={screenWidth}
-    height={220}
-    yAxisLabel="$"
-    chartConfig={chartConfig}
-    verticalLabelRotation={30}
-  />
-)
+    return (
+      <View>
+        <BarChart
+          data={data}
+          width={screenWidth}
+          height={height}
+          chartConfig={chartConfig}
+        />
+      </View>
+    );
+  }
 }
-}
-export default Bar;
