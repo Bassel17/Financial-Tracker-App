be7863aa75cd0164d0f586029fc7986faaada151
rania235 <rania.obeid1996@gmail.com>
2020-04-08 13:10:12 +0300

    creating graphs

--- Frontend/src/pages/ReportPage.js
+++ Frontend/src/pages/ReportPage.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import { Header ,ButtonGroup } from 'react-native-elements';
 import {View} from 'react-native';
+import { Button } from 'react-native';
 
 import Bar from '../components/Bar';
-//import PieChart from '../components/PieChart';
+import Pie from '../components/Pie';
+import LineChartComponent from '../components/LineChart';
+//import { LineChart } from 'react-native-chart-kit';
 
 export default class ReportPage extends React.Component {
+    
+
+  onPressLearnMore(){
+    alert('400$')
+  }
     constructor(props){
         super(props);
         this.state = {
-            selectedIndex: 0
+            selectedIndex: 0,
+            selectedGraphindex: 0,
+           
         }
     }
 
     updateIndex = (selectedIndex) => {
         this.setState({selectedIndex})
       }
+      updateGraphIndex = (selectedGraphindex) => {
+        if(this.state.selectedGraphindex === 0)
+          this.setState({selectedGraphindex : 1});
+        else this.setState({selectedGraphindex:0});
+      }
+      
       render(){
         const buttons = ["EXPENSES","INCOME"]
-        const { selectedIndex } = this.state;
+        const Graphbuttons = ["BarChart","LineChart"]
+       // const Graph = ["BarChart","LineChart"]
+        
+        
+        const { selectedIndex,selectedGraphindex } = this.state;
         return(
             <View style={{
                 height:"100%",
@@ -30,18 +50,43 @@ export default class ReportPage extends React.Component {
               <Header
                     placement="left"
                     leftComponent={{ icon: 'menu', color: '#fff' }}
-                     centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+                     centerComponent={{ text: 'Report', style: { color: '#fff' } }}
                      rightComponent={{ icon: 'home', color: '#fff' }}
                       />
+       
                 <ButtonGroup
                     onPress={this.updateIndex}
                     selectedIndex={selectedIndex}
                     buttons={buttons}
                     containerStyle={{width: "100%"}}
                 />
-               
-               <Bar index={this.state.selectedIndex}/>
-               
+                 
+
+         <Button
+         onPress={this.onPressLearnMore}
+         title="Balance"
+         color="grey"
+         buttonStyle={{
+           width:"30%",
+           paddingTop:"30%"  
+          
+         }}
+        
+        />
+        
+
+           <ButtonGroup
+                    onPress={this.updateGraphindex}
+                    selectedIndex={ selectedGraphindex}
+                    buttons={ Graphbuttons}
+                    containerStyle={{width: "100%"}}
+                />
+                
+       
+
+
+                {selectedGraphindex ===0 ? <Bar index={this.state.selectedIndex}/> : <LineChartComponent index={this.state.selectedGraphindex}/>}  
+            
                 </View>
         );
     }
